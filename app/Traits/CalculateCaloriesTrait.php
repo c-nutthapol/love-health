b9c4bc888166d5de1e192a5b65a89a99fc4fdb9a
Nutthapol Chuenbunchom <nutthapol.chue@gmail.com>
2023-03-24 01:26:31 +0700

    Create CalculateCaloriesTrait.php

---
+++ app/Traits/CalculateCaloriesTrait.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Traits;
+
+trait CalculateCaloriesTrait
+{
+    /**
+     * *|MARCADOR_CURSOR|*
+     *
+     * @param weight Your weight in kilograms
+     * @param height in cm
+     * @param age Your age in years
+     */
+    public function bmr($weight, $height, $age, $gender)
+    {
+        if ($gender == 'male') {
+            $bmr = 88.362 + (13.397 * $weight) + (4.799 * $height) - (5.677 * $age);
+        } else {
+            $bmr = 447.593 + (9.247 * $weight) + (3.098 * $height) - (4.330 * $age);
+        }
+        return $bmr;
+    }
+
+    public function burned($exerciseTime, $burned_hour)
+    {
+        return $exerciseTime * ($burned_hour / 60);
+    }
+
+    public function calories($bmr, $consumed, $burned, $mentalHealth)
+    {
+
+        if ($mentalHealth == 'good') {
+            $burned *= 1.1;
+        } else if ($mentalHealth == 'poor') {
+            $burned *= 0.9;
+        }
+
+        // Calculate BMR and total calories for the day
+        $total = $bmr + $consumed - $burned;
+
+        if ($total < 1200) {
+            return 'คุณต้องเพิ่มปริมาณแคลอรี่ในแต่ละวัน ' . $total;
+        } else if ($total > 2500) {
+            return 'คุณต้องลดปริมาณแคลอรี่ในแต่ละวัน ' . $total;
+        } else {
+            return 'ปริมาณแคลอรี่ในแต่ละวันของคุณอยู่ในช่วงที่แนะนำ ' . $total;
+        }
+    }
+}
